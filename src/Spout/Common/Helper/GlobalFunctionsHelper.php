275aaf937ade841015e07a89e8f381db35a48730
Wilson Glasser <wilsonglasser@gmail.com>
2024-11-28 18:15:34 -0300

    Update GlobalFunctionsHelper.php
    Fix for php 8.2+

--- src/Spout/Common/Helper/GlobalFunctionsHelper.php
+++ src/Spout/Common/Helper/GlobalFunctionsHelper.php
@@ -246,7 +246,7 @@ class GlobalFunctionsHelper
      * @param string|null $suffix
      * @return string
      */
-    public function basename($path, $suffix = null)
+    public function basename($path, $suffix = '')
     {
         return basename($path, $suffix);
     }
